5de9644e5470d27781f2282fd0db8772a10ea45a
🌵 <23.251-02.shinguu@visualarts.ac.jp>
2024-12-12 17:59:16 -1200

    いにしゃるこみっと！

---
+++ js/trap.js
@@ -0,0 +1,44 @@
+const life = document;
+
+class directionClass{
+    r = "right";
+    l = "left";    
+    t = "top";
+    b = "bottom";
+}
+
+direction = new directionClass;
+const mainArea = life.getElementById("mainArea");
+
+function aurora(){    
+    obj = life.createElement("div");
+    obj.className = "生きとし生ける者";
+    mainArea.appendChild(obj);
+    SimpleMove(obj,direction.r);    
+    var ipData = fetch('https://knee-festa.vercel.app/member?callback')
+    .then(res => res.json())
+    .then(json => console.log(json.ip))
+}
+aurora();
+
+function SimpleMove(element,direction,speed){
+    switch(direction){
+        case direction.r:
+
+            break;
+        case direction.l:
+
+            break;
+        case direction.t:
+
+            break;
+        case direction.b:
+
+            break;
+
+            
+            // //localStorage.setItem("key",ipData);
+            // console.log(ipData);
+            
+    }
+}
