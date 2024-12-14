e908032a39c4318170e2e9b2e7757777a99fa200
《踊りたいだけである》 <misosoftware.1024@gmail.com>
2024-12-14 16:37:20 +0900

    なんもわかってないけどいったんあげてみるやつ

--- js/trap.js
+++ js/trap.js
@@ -11,28 +11,29 @@ direction = new directionClass;
 const mainArea = life.getElementById("mainArea");
 var ipData;
 
+window.onload = setTimeout(aurora,1000);
 function aurora(){    
     obj = life.createElement("div");
     obj.className = "生きとし生ける者";
     mainArea.appendChild(obj);
     SimpleMove(obj,direction.r);                
+    var key = 'key'
+    var action = 'upvoteProduct'
+    var namespace = 'StultaAfero.gift.site'
+    var options = { startNumber: 0, behavior: 'vote' } //see options list
+    
+    counterApi.read(key, action, namespace, options, function(err, res){
+    alert('Current number of votes for this: '+ res.value);
+    //do something ...
+    })
+    
+    //increase a counter
+    counterApi.increment(key, action, namespace, options, function(err, res){
+    alert('New upvote cout is now '+ res.value);
+    //do something ...
+    })
+    
 }
-var key = 'key'
-var action = 'upvoteProduct'
-var namespace = 'StultaAfero.gift.site'
-var options = { startNumber: 300, behavior: 'vote' } //see options list
-
-counterApi.read(key, action, namespace, options, function(err, res){
-alert('Current number of votes for this: '+ res.value);
-//do something ...
-})
-
-//increase a counter
-counterApi.increment(key, action, namespace, options, function(err, res){
-alert('New upvote cout is now '+ res.value);
-//do something ...
-})
-aurora();
 
 function SimpleMove(element,direction,speed){
     switch(direction){
